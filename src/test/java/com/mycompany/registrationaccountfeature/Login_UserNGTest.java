4c74e8efdfd0ca69575127417eb5461a638a51ef
Student <Student@LabVM2047644.ahv04kos4wgutoemvjezqa3afg.jnbx.internal.cloudapp.net>
2026-04-13 16:59:14 +0200

    First commit

--- src/test/java/com/mycompany/registrationaccountfeature/Login_UserNGTest.java
+++ src/test/java/com/mycompany/registrationaccountfeature/Login_UserNGTest.java
@@ -48,7 +48,7 @@ public class Login_UserNGTest {
         boolean result = instance.checkUserName(userName);
         assertEquals(result, expResult);
         // TODO review the generated test code and remove the default call to fail.
-        fail("The test case is a prototype.");
+        
     }
 
     /**
